5452c6826727cdd9a2a06568f510cb28a7d04e76
Ragi271994 <nahateragi.27@gmail.com>
2019-02-25 16:02:31 +0530

    My commit

---
+++ todopro/todopro/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from accounts.views import index
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^$', index, name="TodoList"),
+]
